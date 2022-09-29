c068ca9154d9c1729f09cd0243bd0abadb86e7d4
Jon Cram <webignition@gmail.com>
2022-09-29 14:56:27 +0100

    Create empty project

---
+++ .php-cs-fixer.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR12' => true,
+    '@PhpCsFixer' => true,
+    'concat_space' => [
+        'spacing' => 'one',
+    ],
+    'trailing_comma_in_multiline' => false,
+    'php_unit_internal_class' => false,
+    'php_unit_test_class_requires_covers' => false,
+    'declare_strict_types' => true,
+    'types_spaces' => [
+        'space_multiple_catch' => 'single',
+    ],
+])->setFinder($finder);
